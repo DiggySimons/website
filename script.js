62497346967e8cdd11927356684f0e509492a3b2
DiggySimons <stonerman824@gmail.com>
2024-02-17 14:57:55 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.getElementById('postForm').addEventListener('submit', async (e) => {
+document.getElementById('postForm').addEventListener('submit'), async (e) => {}
     e.preventDefault();
     const title = document.getElementById('title').value;
     const content = document.getElementById('content').value;
